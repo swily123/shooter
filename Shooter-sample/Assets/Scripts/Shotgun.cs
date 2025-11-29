eb1061d92d496fce35aa96c84a75f52cfd7d74f4
swily123 <92625491+swily123@users.noreply.github.com>
2025-11-29 19:15:56 +0500

    add physical interaction

--- Shooter-sample/Assets/Scripts/Shotgun.cs
+++ Shooter-sample/Assets/Scripts/Shotgun.cs
@@ -2,17 +2,38 @@
 
 public class Shotgun : MonoBehaviour
 {
+    [Header("Gun")]
     [SerializeField] private float _damage;
     [SerializeField] private LayerMask _layerMask;
     [SerializeField] private float _maxDistance;
+    [SerializeField] private float _impactForce;
+    
+    [Header("Effects")]
+    [SerializeField] private Transform _decal;
+    [SerializeField] private ShootEffect _shotEffect;
+    [SerializeField] private float _decalOffset;
+    [SerializeField] private Animator _shotAnimator;
     
     public void Shoot(Vector3 startPoint, Vector3 direction)
     {
-        if (Physics.Raycast(startPoint, direction, out RaycastHit hit, _maxDistance, _layerMask, QueryTriggerInteraction.Ignore))
+        if (Physics.Raycast(startPoint, direction, out RaycastHit hitInfo, _maxDistance, _layerMask, QueryTriggerInteraction.Ignore))
         {
-            var health = hit.collider.GetComponentInParent<Health>();
-
+            _shotAnimator.SetTrigger("ShootAndReload");
+            var decal = Instantiate(_decal, hitInfo.transform);
+            decal.position = hitInfo.point + hitInfo.normal * _decalOffset;
+            decal.LookAt(hitInfo.point);
+            decal.Rotate(Vector3.up, 180f, Space.Self);
+            
+            var health = hitInfo.collider.GetComponentInParent<Health>();
             health?.TakeDamage(_damage);
+            
+            var victimBody  = hitInfo.rigidbody;
+            if (victimBody)
+            {
+                victimBody.AddForceAtPosition(direction * _impactForce, hitInfo.point, ForceMode.Force);
+            }
+            
+            _shotEffect.Perform();
         }
     }
 }
