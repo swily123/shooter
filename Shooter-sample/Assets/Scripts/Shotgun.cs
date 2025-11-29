850744996d8af938322aee4bf392039a95fb8bc0
swily123 <92625491+swily123@users.noreply.github.com>
2025-11-29 17:07:16 +0500

    add materials

--- Shooter-sample/Assets/Scripts/Shotgun.cs
+++ Shooter-sample/Assets/Scripts/Shotgun.cs
@@ -3,9 +3,16 @@
 public class Shotgun : MonoBehaviour
 {
     [SerializeField] private float _damage;
+    [SerializeField] private LayerMask _layerMask;
+    [SerializeField] private float _maxDistance;
     
     public void Shoot(Vector3 startPoint, Vector3 direction)
     {
-                
+        if (Physics.Raycast(startPoint, direction, out RaycastHit hit, _maxDistance, _layerMask, QueryTriggerInteraction.Ignore))
+        {
+            var health = hit.collider.GetComponentInParent<Health>();
+
+            health?.TakeDamage(_damage);
+        }
     }
 }
